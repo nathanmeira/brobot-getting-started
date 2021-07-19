2c02b515b5eefeb3e3fe6861aed4575dd7d23043
matheus moreira ferreira <moreira.mferreira@gmail.com>
2021-07-19 17:04:08 -0300

    improving routes

--- .history/challenge01/config/routes_20210719165702.rb
+++ .history/challenge01/config/routes_20210719165702.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :person
+  get 'person/index'
+  get 'person/show'
+  get 'person/new'
+  get 'person/edit'
+  get 'person/full_name'
+  get 'person/imc'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
